5b3fba6a378d4f5c6347665eea2eb5a7c8c228da
James Appello <jamesappello@gmail.com>
2019-10-07 20:15:54 -0400

    update

---
+++ data/songPicker.js
@@ -0,0 +1,18 @@
+// logic for recommending a sog!
+
+// Q1:: how much time do spend on your phone on any given day (approximate guess*)
+
+// Q2: When you listen to music, how log would each "sesh" be?
+    // ESLE IF:::: what do you do primarily when streaming music???? *******
+            // WHAT DOES MUSIC DO FOR YOU?
+
+// Q1.ans - Q2.ans === time to determine GAP
+//===================================
+
+//  ~~   START    ~~
+// define MICRO_variables -> waves/frequencies/energy
+
+// define MACRO_variables -> constants
+const daytimeHRS = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+
+
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = (app) => {
+    /*
+    app.get("/login", (req,resp) => {
+        // ******* FIX THIS ISHHHHHHHH!
+        resp.sendFile(path.join(__dirname, ""));
+    });
+    */
+    app.get("/", (req,resp) => {
+        // console.log(path.join(__dirname, '../views'))
+        resp.render(path.join(__dirname, "../views/index"));
+    });
+}
